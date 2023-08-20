67d420dfc9a81cf22a38636494d4ebf7a92d1f40
osheanman <60367639+osheanman@users.noreply.github.com>
2023-08-20 12:28:28 -0700

    Time: 204 ms (100%), Space: 221.2 MB (100%) - LeetHub

--- 2355-maximum-consecutive-floors-without-special-floors/2355-maximum-consecutive-floors-without-special-floors.rb
+++ 2355-maximum-consecutive-floors-without-special-floors/2355-maximum-consecutive-floors-without-special-floors.rb
@@ -6,15 +6,15 @@ def max_consecutive(bottom, top, special)
     if special.length == 0
         return top - bottom + 1
     end
-    arr = special.sort
+    special.sort!
     
-    maximum = arr[0] - bottom
-    for i in 0..arr.length-2 do
-        gap = arr[i+1] - arr[i] - 1
+    maximum = special[0] - bottom
+    for i in 0..special.length-2 do
+        gap = special[i+1] - special[i] - 1
         comp = [maximum, gap]
         maximum = comp.max
     end
-    gap = top - arr[arr.length-1]
+    gap = top - special[special.length-1]
     comp = [maximum, gap]
     maximum = comp.max
     return maximum
